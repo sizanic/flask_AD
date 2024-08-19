711da6637183059b5b13e3ccfefc5bcdf07551ef
sizanic <47307622+sizanic@users.noreply.github.com>
2024-08-20 01:51:24 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -8,10 +8,6 @@ import sqlite3
 app = Flask(__name__)                                                                                                                  
 app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'  # Clé secrète pour les sessions  
 
-# Fonction pour créer une clé "authentifie" dans la session utilisateur
-def est_authentifie():
-    return session.get('authentifie')
-
 @app.route('/')
 def hello_world():
     return render_template('hello.html')
