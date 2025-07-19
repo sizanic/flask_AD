99d95ad4a6061e38dea1b5bcd6f64361a6ad6219
sizanic <47307622+sizanic@users.noreply.github.com>
2025-07-19 15:46:34 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -19,8 +19,8 @@ app = Flask(__name__)
 API_KEYS = deque([
 #    "je5gAJSBwdYRTKGUuJA7",        # vPasteStream    // OK  01/09/2025 03:50
 #    "TKftCdvxhrU2abyNnpNQ",        # clyrkakenta     // OK  09/08/2025 12:16 (shanbox.izanic)
-    "YTlkoKj3gxGHQ9XT9mHk"          # shansblock     // OK  16/08/2025 01:57
-#    "JpldxPIQ8ng7camNNH3n"         # sizanic         // OK  26/07/2025 19:35
+    "YTlkoKj3gxGHQ9XT9mHk",          # shansblock     // OK  16/08/2025 01:57
+    "JpldxPIQ8ng7camNNH3n"         # sizanic         // OK  26/07/2025 19:35
 ])
 last_request_time = time.time()
 
