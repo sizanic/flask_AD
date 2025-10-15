df78a0912ff83093f01b3334fe06847805d06f39
sizanic <47307622+sizanic@users.noreply.github.com>
2025-10-15 14:59:44 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -19,7 +19,7 @@ app = Flask(__name__)
 LOG = deque()
 
 API_KEYS = deque([
-    "9wHK4DK1NggY824vA7C0",        # shansblock      //   04/11/2025 20:23
+    "mnnrYj3Yjv93O8FE7xpX",        # shansblock      //   04/11/2025 20:23
    # "CGDxEacF7zmAeQhsmgIa",        # clyrkakenta     //   28/09/2025 00:17 (shanbox.izanic)
     "HgvndfiBLypKfULAG670",        # vPasteStream    //   06/01/2026 02:50
     "OhZ6OkVdAoKmvMBdkc01"         # sizanic         //   21/10/2025 17:14
