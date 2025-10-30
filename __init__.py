40013c5c3b6ac2e02aed9fd3880f5601c5b0c6df
sizanic <47307622+sizanic@users.noreply.github.com>
2025-10-30 10:06:59 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -20,7 +20,7 @@ LOG = deque()
 
 API_KEYS = deque([
     "p7mfrhXOVNN4g8mivKVT",        # shansblock      //   04/11/2025 20:23
-    "CGDxEacF7zmAeQhsmgIa",        # clyrkakenta     //   22/11/2025 21:45 (shanbox.izanic)
+    "lASTGqzZIMY1DCcrZHGv",        # clyrkakenta     //   22/11/2025 21:45 (shanbox.izanic)
     "u9QTPP3YBkGbk5gWh5Bp",        # vPasteStream    //   06/01/2026 02:50
     "XQ1x3y1yl0zOTpv3Cji6"         # sizanic         //   21/11/2025 15:51
 ])
