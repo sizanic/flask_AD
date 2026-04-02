a50965ed676d89e528e200c18415e3afdba0cf8d
sizanic <47307622+sizanic@users.noreply.github.com>
2026-04-02 21:05:10 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -24,7 +24,7 @@ API_KEYS = deque([
     "rXRKYKuaZ8nZ2SoVHFBq",        # iosShanStream    //   11/04/2026 21:54
     "jjabS1bd4uF2POT8Z7bT",        # shansblock       //   01/05/2026 23:34
     "UiMkYWO5VKnBJfkAPczz",        # clyrkakenta      //   08/04/2026 17:14 (shanbox.izanic)
-    "d15ZTiSjWV2myBX32371"         # vPasteStream     //   11/04/2026 21:55
+    "JbqXVT2XGm9LKNAV61b5"         # vPasteStream     //   11/04/2026 21:55
 ])
 last_request_time = 0
 
